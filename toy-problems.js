c6216fed0a8ab63067b4286184ce582ee75b7d89
jasondawson <jason.c.dawson@gmail.com>
2015-01-21 09:14:40 -0700

    added simpleSymbols

--- toy-problems.js
+++ toy-problems.js
@@ -15,3 +15,16 @@ var factorial = function(num) {
     }
   return result;
 };
+
+//Write a function called simpleSymbols that takes in a string parameter and determines if it is an acceptable sequence by either returning the string true or false. The str parameter will be composed of + and = symbols with several letters between them (i.e. ++d+=====+c++===a) and for the string to be true each letter must be surrounded by a + symbol. So the string to the left would be false. The string will not be empty and will have at least one letter.
+
+var simpleSymbols = function(str) {
+	for (var i = 0; i < str.length; i++) {
+		if (str[i] !== '+' && str[i] !== '=') {
+			if (str[i - 1] !== '+' || str[i + 1] !== '+') {
+				return false;
+			}
+		}
+	}
+	return true;
+};
