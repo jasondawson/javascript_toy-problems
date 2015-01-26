8b95f9c60f1dcdb3e14137bc9d4f838891794315
jasondawson <jason.c.dawson@gmail.com>
2015-01-26 09:26:39 -0700

    added ABCheck

--- toy-problems.js
+++ toy-problems.js
@@ -53,4 +53,18 @@ var noRepeats = function(str) {
     }
   }
   return false;
+};
+
+
+// Write a function called ABCheck that takes a string parameter and returns the string true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (e.g "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false.
+
+var ABCheck = function(str) {
+	str = str.toLowerCase();
+	for (var i = 0; i < str.length; i++) {
+		if (str[i] === 'a')
+			if (str[i - 4] === 'b' || str[i + 4] === 'b') {
+				return 'true';
+			}
+	}
+	return 'false';
 }
