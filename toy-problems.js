8cf844d4f60a1ecb0bafce9d41e4ad39af531a89
jasondawson <jason.c.dawson@gmail.com>
2015-02-09 09:44:04 -0700

    added objectArrayLookup

--- toy-problems.js
+++ toy-problems.js
@@ -174,3 +174,87 @@ var flatten = function(arr) {
 	}
 	return newArr;
 }
+
+var animals = [
+    {
+        name: 'Dog',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Carnivora',
+        family: 'Canidae',
+        species: 'Canis lupus'
+    },
+    {
+        name: 'Cat',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Carnivora',
+        family: 'Felidae',
+        species: 'Felis catus'
+    },
+    {
+        name: 'Hippopotamus',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Artiodactyla',
+        family: 'Hippopotamidae',
+        species: 'Hippopotamus amphibius'
+    },
+    {
+        name: 'Playtpus',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Monotremata',
+        family: 'Ornithorhynchidae',
+        species: 'Ornithorhynchus anatinus'
+    },
+    {
+        name: 'Lizard',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Reptilia',
+        order: 'Squamata',
+        family: 'Agamidae',
+        species: 'Pogona vitticeps'
+    },
+    {
+        name: 'Blackbird',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Aves',
+        order: 'Passeriformes',
+        family: 'Turdidae',
+        species: 'Turdus merula'
+    }
+];
+
+//given the above object, write a function that returns an object whose keys are the fields found above and whose values are objects containing all the values as keys and a count for how many times that value occurs
+
+//e.g.
+//{
+//    kingdom: {Animalia: 6}
+//    phylum: {Chordata: 6},
+//    class: {Mammalia: 4, Reptilia: 1, Aves: 1}
+//}
+
+var objectArrayLookup = function (arr) {
+    var lookup = {};
+    for (var i = 0; i < arr.length; i++) {
+        for (var key in arr[i]) {
+        	if (!lookup[key]) {
+            	lookup[key] = {};
+        	} 
+        	if (!lookup[key][arr[i][key]]) {
+        		lookup[key][arr[i][key]] = 1;
+        	}
+        	else {
+            	lookup[key][arr[i][key]]++;
+        	}
+		}
+	}
+	return lookup;
+}
