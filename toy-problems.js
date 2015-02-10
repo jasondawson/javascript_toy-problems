d764f7915e6cd433e959305931283dca6412d3fc
jasondawson <jason.c.dawson@gmail.com>
2015-02-10 11:21:23 -0700

    added objectArraySearch

--- toy-problems.js
+++ toy-problems.js
@@ -258,3 +258,36 @@ var objectArrayLookup = function (arr) {
 	}
 	return lookup;
 }
+
+//create a function that takes an array of objects as an argument and a string that represents a property of an object contained in the array and returns an object whose keys are the properties and whose values are arrays of objects that match that property.
+
+//e.g.
+/*var arr = [
+    {name: 'Joe Montana', team: '49ers'}, 
+    {name: 'Jerry Rice', team: '49ers'}, 
+    {name: 'Steve Young', team: '49ers'},
+    {name: 'Tony Romo', team: 'Cowboys'},
+    {name: 'Fred Biletnikoff', team: 'Raiders'},
+    {name: 'Dick Butkus', team: 'Bears'},
+    {name: 'Emmitt Smith', team: 'Cowboys'},
+];
+
+//should return:
+{ 
+    '49ers': [{name: 'Joe Montana', team: '49ers'}, {name: 'Jerry Rice', team: '49ers'}, {name: 'Steve Young', team: '49ers'}],
+    'Cowboys': [{name: 'Tony Romo', team: 'Cowboys'}, {name: 'Emmitt Smith', team: 'Cowboys'}],
+    'Bears': [{name: 'Dick Butkus', team: 'Bears'}],
+    'Raiders': [{name: 'Fred Biletnikoff', team: 'Raiders'}]
+}*/
+
+var objectArraySearch = function(arr, str) {
+    var tempArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        for (var key in arr[i]) {
+            if (arr[i][key] === str) {
+                tempArr.push(arr[i]);
+            }
+        }
+        }
+        return tempArr;
+    }
